5c006eb8922ebb15df638649d4bb042d6deabea9
BrainAxe <tanzimrizwan@gmail.com>
2021-07-02 01:12:16 +0600

    added Header component

--- my-first-nuxt-app/layouts/default.vue
+++ my-first-nuxt-app/layouts/default.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
+    <Header/>
     <nuxt/>
   </div>
 </template>
+<script>
+import Header from '@/components/Header.vue';
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
 
 <style>
 
--- my-first-nuxt-app/layouts/users.vue
+++ my-first-nuxt-app/layouts/users.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
+    <Header />
     <header>
       USERS SECTION
     </header>
     <nuxt/>
   </div>
 </template>
+<script>
+import Header from '@/components/Header.vue';
+
+export default {
+  components: {
+    Header
+  }  
+}
+</script>
 
 <style>
 
