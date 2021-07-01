fb5529c6a798c7bbbc388cf93be93e6f4a9092f6
BrainAxe <tanzimrizwan@gmail.com>
2021-07-01 14:40:16 +0600

    updated some configs

--- my-first-nuxt-app/nuxt.config.js
+++ my-first-nuxt-app/nuxt.config.js
@@ -2,9 +2,6 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'my-first-nuxt-app',
-    htmlAttrs: {
-      lang: 'en'
-    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -29,11 +26,22 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
   ],
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
