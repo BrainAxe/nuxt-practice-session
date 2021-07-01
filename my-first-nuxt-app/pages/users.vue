4c0346fdc470c8ead25ca8888320991973d170eb
BrainAxe <tanzimrizwan@gmail.com>
2021-07-01 20:08:11 +0600

    added nested routing

---
+++ my-first-nuxt-app/pages/users.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <input v-model="userId" type="text">
+    <button @click="onLoadUser">Load User</button>
+    <nuxt-child />
+  </div>
+</template>
+<script>
+export default {
+  name: '',
+  components: {},
+  data() {
+    return {
+      userId: ''
+    };
+  },
+  computed: {},
+  watch: {},
+  mounted() {},
+  methods: {
+    onLoadUser() {
+      this.$router.push('/users/' + this.userId);
+    }
+  },
+};
+</script>
--- my-first-nuxt-app/pages/users/index.vue
+++ my-first-nuxt-app/pages/users/index.vue
@@ -1,27 +1,4 @@
 <template>
-  <div>
-    <input v-model="userId" type="text">
-    <button @click="onLoadUser">Load User</button>
-  </div>
+  <p>Please enter id and click the button.</p>
 </template>
-<script>
-export default {
-  name: '',
-  components: {},
-  data() {
-    return {
-      userId: ''
-    };
-  },
-  computed: {},
-  watch: {},
-  mounted() {},
-  methods: {
-    onLoadUser() {
-      this.$router.push('/users/' + this.userId);
-    }
-  },
-};
-</script>
-<style scoped>
-</style>
+
