02b1fbe63725fc533fdd9690bf799a465fa2d674
BrainAxe <tanzimrizwan@gmail.com>
2021-07-01 17:35:50 +0600

    added links and navigation

--- my-first-nuxt-app/pages/users/index.vue
+++ my-first-nuxt-app/pages/users/index.vue
@@ -1,5 +1,27 @@
 <template>
-  <h1>
-    The users view
-  </h1>
+  <div>
+    <input v-model="userId" type="text">
+    <button @click="onLoadUser">Load User</button>
+  </div>
 </template>
+<script>
+export default {
+  name: '',
+  components: {},
+  data() {
+    return {
+      userId: ''
+    };
+  },
+  computed: {},
+  watch: {},
+  mounted() {},
+  methods: {
+    onLoadUser() {
+      this.$router.push('/users/' + this.userId);
+    }
+  },
+};
+</script>
+<style scoped>
+</style>
