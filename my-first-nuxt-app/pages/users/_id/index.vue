86fe6449ba604c828a5d256fb5eb5d2f31a0fa49
BrainAxe <tanzimrizwan@gmail.com>
2021-07-01 17:09:34 +0600

    added route with dynamic path

---
+++ my-first-nuxt-app/pages/users/_id/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>A single user, with ID: {{ $route.params.id }}</h1>
+</template>
+<script>
+export default {
+  name: '',
+  components: {},
+  // data() {
+  //   return {}
+  // },
+  computed: {},
+  watch: {},
+  mounted() {},
+  methods: {},
+}
+</script>
+<style scoped></style>
---
+++ my-first-nuxt-app/pages/users/_id/more-details.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Here goes more details</h1>
+</template>
