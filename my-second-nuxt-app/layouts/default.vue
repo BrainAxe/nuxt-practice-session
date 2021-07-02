f6eebdc5bc95b14eab731e86b97be521d24598ab
BrainAxe <tanzimrizwan@gmail.com>
2021-07-02 13:23:38 +0600

    added slidebar

--- my-second-nuxt-app/layouts/default.vue
+++ my-second-nuxt-app/layouts/default.vue
@@ -1,8 +1,27 @@
 <template>
   <div>
+    <TheHeader @sidenavToggle="displaySidenav = !displaySidenav" />
+    <TheSidenav :show="displaySidenav" @close="displaySidenav = false"/>
     <nuxt/>
   </div>
 </template>
+
+<script>
+import TheHeader from '@/components/Navigation/TheHeader.vue';
+import TheSidenav from '@/components/Navigation/TheSidenav.vue';
+
+export default {
+  components: {
+    TheHeader,
+    TheSidenav
+  },
+  data() {
+    return {
+      displaySidenav: false
+    }
+  }
+}
+</script>
 
 <style>
   html {
