9292abc0010b175d662f74c44a8d3f84300aa445
BrainAxe <tanzimrizwan@gmail.com>
2021-07-02 13:04:36 +0600

    spliting pages into components

--- my-second-nuxt-app/pages/index.vue
+++ my-second-nuxt-app/pages/index.vue
@@ -4,109 +4,75 @@
       <h1>Get the latest tech news!</h1>
     </section>
     <section class="featured-posts">
-      <nuxt-link :to="'/posts/' + 1" class="post-preview">
-        <article>
-          <div class="post-thumbnail" style="background-image: url('https://i.dawn.com/primary/2020/04/5e8b78912bd68.jpg')">
-
-          </div>
-          <div class="post-content">
-            <h1>Post Title</h1>
-            <p>Preview Text</p>
-          </div>
-        </article>
-      </nuxt-link>
-       <nuxt-link :to="'/posts/' + 2" class="post-preview">
-        <article>
-          <div class="post-thumbnail" style="background-image: url('https://i.dawn.com/primary/2020/04/5e8b78912bd68.jpg')">
-
-          </div>
-          <div class="post-content">
-            <h1>Post Title 2</h1>
-            <p>Preview Text 2</p>
-          </div>
-        </article>
-      </nuxt-link>
+      <PostPreview
+        id="1"
+        thumbnail="https://i.dawn.com/primary/2020/04/5e8b78912bd68.jpg"
+        title="Hello there!"
+        preview-text="This is my first post"
+      />
+      <PostPreview
+        id="2"
+        thumbnail="https://i.dawn.com/primary/2020/04/5e8b78912bd68.jpg"
+        title="Hello there - the second time!"
+        preview-text="This is my second post"
+      />
+      <PostPreview
+        id="3"
+        thumbnail="https://i.dawn.com/primary/2020/04/5e8b78912bd68.jpg"
+        title="Hello there - the third time!"
+        preview-text="This is my third post"
+      />
     </section>
   </div>
 </template>
 
 <script>
-export default {}
+import PostPreview from '@/components/Posts/PostPreview.vue'
+
+export default {
+  components: {
+    PostPreview,
+  },
+}
 </script>
 
 <style scoped>
-  .intro {
-    height: 300px;
-    position: relative;
-    padding: 30px;
-    box-sizing: border-box;
-    background-position: center;
-    background-size: cover;
-  }
+.intro {
+  height: 300px;
+  position: relative;
+  padding: 30px;
+  box-sizing: border-box;
+  background-position: center;
+  background-size: cover;
+}
 
+.intro h1 {
+  position: absolute;
+  top: 10%;
+  left: 5%;
+  width: 90%;
+  font-size: 1.5rem;
+  color: black;
+  background-color: rgb(211, 211, 211);
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px black;
+  box-sizing: border-box;
+  border: 1px solid black;
+}
+
+@media (min-width: 768px) {
   .intro h1 {
-    position: absolute;
-    top: 10%;
-    left: 5%;
-    width: 90%;
-    font-size: 1.5rem;
-    color: black;
-    background-color: rgb(211, 211, 211);
-    padding: 10px;
-    border-radius: 10px;
-    box-shadow: 3px 3px 3px black;
-    box-sizing: border-box;
-    border: 1px solid black;
+    font-size: 2rem;
   }
+}
 
-  @media (min-width: 768px) {
-    .intro h1 {
-      font-size: 2rem;
-    }
-  }
-
-  .featured-posts {
-    display: flex;
-    padding: 20px;
-    box-sizing: border-box;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: center;
-  }
-
-  .post-preview {
-    border: 1px solid #ccc;
-    box-shadow: 0 2px 2px #ccc;
-    background-color: white;
-    width: 90%;
-  }
-
-  a {
-    text-decoration: none;
-    color: black;
-  }
-
-  @media (min-width: 850px) {
-    .post-preview {
-      width: 400px;
-      margin: 10px;
-    }
-  }
-
-  .post-thumbnail {
-    width: 100%;
-    height: 200px;
-    background-position: center;
-    background-size: cover;
-  }
-
-  .post-content {
-    padding: 10px;
-    text-align: center;
-  }
-
-  a:hover .post-content,
-  a:active .post-content {
-    background-color: #ccc;
-  }
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
 </style>
